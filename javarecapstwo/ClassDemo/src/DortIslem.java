df3d35511a7b0eccfcc0f4f2001e953ac82a1c0c
MuhammetGunes <91566366+MuhammetGunes@users.noreply.github.com>
2022-09-28 12:25:38 +0300

    Add files via upload

---
+++ javarecapstwo/ClassDemo/src/DortIslem.java
@@ -0,0 +1,15 @@
+
+public class DortIslem {
+	public int Topla(int sayi, int sayi2) {
+		return sayi + sayi2;
+	}
+	public int Cikar(int sayi, int sayi2) {
+		return sayi - sayi2;
+	}
+	public int Carp(int sayi, int sayi2) {
+		return sayi * sayi2;
+	}
+	public int Bol(int sayi, int sayi2) {
+		return sayi / sayi2;
+	}
+}
